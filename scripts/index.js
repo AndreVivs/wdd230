d2d2597a59629d9e274a9a405012b62ed40130b2
Andrea Ramos <andrevivs87@gmail.com>
2025-01-16 15:08:15 -0700

    team activity advance

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+// Year and last modification
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+const lastModifiedDate = document.lastModified;
+document.getElementById("lastModified").textContent =
+  "Last modified: " + lastModifiedDate;
+
+// Hamburger menu
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+  hamButton.classList.toggle("open");
+});
+
+// Dark mode
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+
+modeButton.addEventListener("click", () => {
+  if (modeButton.textContent.includes("🕶️")) {
+    main.style.background = "#000";
+    main.style.color = "#fff";
+    header.style.background = "#000";
+    footer.style.background = "#000";
+    modeButton.textContent = "🔆";
+  } else {
+    main.style.background = "#eee";
+    main.style.color = "#000";
+    header.style.background = "#987144";
+    footer.style.background = "#987144";
+    modeButton.textContent = "🕶️";
+  }
+});
