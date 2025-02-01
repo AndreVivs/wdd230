1479c9410eca02ef40e8772757613e58eecf2256
Andrea Ramos <andrevivs87@gmail.com>
2025-02-01 16:40:44 -0700

    hamburger menu working

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -2,3 +2,12 @@
 const lastModifiedDate = document.lastModified;
 document.getElementById("lastModified").textContent =
   "Last modified: " + lastModifiedDate;
+
+// Hamburger menu
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+  hamButton.classList.toggle("open");
+});
