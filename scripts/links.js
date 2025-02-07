8e59d1a5f5cbc028bb87c1fde2ea27fbd93afd18
Andrea Ramos <andrevivs87@gmail.com>
2025-02-07 16:31:56 -0700

    linksURL added

--- scripts/links.js
+++ scripts/links.js
@@ -1,5 +1,5 @@
 const baseURL = "https://andrevivs.github.io/wdd230/";
-const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
+const linksURL = "https://andrevivs.github.io/wdd230/data/links.json";
 
 async function getLinks() {
   try {
@@ -15,8 +15,9 @@ async function getLinks() {
 
 getLinks();
 
-/*function displayLinks(data) {
+function displayLinks(data) {
   const list = document.querySelector("#links");
+  const week = data.week;
   data.links.forEach((link) => {
     let item = document.createElement("li");
     let a = document.createElement("a");
@@ -25,4 +26,4 @@ getLinks();
     item.appendChild(`${data.week}:${a}`);
     list.appendChild(item);
   });
-}*/
+}
